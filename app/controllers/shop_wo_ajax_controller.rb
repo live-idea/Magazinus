5648164d6d586ed7c574aaab7d5ca51eba9d68a0
unknown <OS@.(none)>
2010-04-11 16:47:48 +0300

    Added show_good method, improved UI a bit

--- app/controllers/shop_wo_ajax_controller.rb
+++ app/controllers/shop_wo_ajax_controller.rb
@@ -1,3 +1,11 @@
 class ShopWoAjaxController < ApplicationController
   layout "shop_wo_ajax"
+
+
+  def show_good
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @good }
+    end
+  end
 end
