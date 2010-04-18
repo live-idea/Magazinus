8c24438a8a0c9c9333a2814892eda0bc02b0bc1e
unknown <OS@.(none)>
2010-04-18 15:03:23 +0300

    Added PayPla config seller email

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -39,4 +40,15 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   config.gem "authlogic"
+
+
+
 end
+
+
+  unless RAILS_ENV == 'production'
+    PAYPAL_ACCOUNT = 'seller_1271512606_biz@gmail.com'
+    ActiveMerchant::Billing::Base.mode = :test
+  else
+    PAYPAL_ACCOUNT = 'paypalaccount@example.com'
+  end
